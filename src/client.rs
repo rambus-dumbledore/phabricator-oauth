15234c1e7e00e960d0c746e40199f4f6f8b6379f
Ilya Volchenkov <lzhepelmen@gmail.com>
2021-05-07 23:15:41 +0300

    Updated oauth2 stable version

--- src/client.rs
+++ src/client.rs
@@ -1,12 +1,13 @@
-use oauth2::basic::{BasicClient, BasicErrorResponse, BasicTokenResponse, BasicTokenType, BasicTokenInspectionResponse};
-use oauth2::{ClientId, ClientSecret, AuthUrl, TokenUrl, RedirectUrl, CsrfToken, AuthorizationCode, AccessToken};
-use oauth2::reqwest::http_client;
-use std::borrow::Cow;
-use oauth2::url::Url;
+use oauth2::{
+    ClientId, ClientSecret, AuthUrl, TokenUrl, RedirectUrl, CsrfToken, AuthorizationCode, AccessToken, StandardRevocableToken,
+    basic::{BasicClient, BasicErrorResponse, BasicTokenResponse, BasicTokenType, BasicTokenIntrospectionResponse, BasicRevocationErrorResponse},
+    reqwest::async_http_client,
+    url::Url,
+};
 use reqwest;
-
-use serde::{Deserialize};
 use serde_json;
+use std::borrow::Cow;
+use serde::{Deserialize};
 
 use crate::user::PhabricatorUser;
 
@@ -22,7 +23,7 @@ struct OAuthResponse<T> {
 pub struct PhabOAuthClient {
     redirect_url: String,
     phabricator_url: String,
-    client: oauth2::Client<BasicErrorResponse, BasicTokenResponse, BasicTokenType, BasicTokenInspectionResponse>
+    client: oauth2::Client<BasicErrorResponse, BasicTokenResponse, BasicTokenType, BasicTokenIntrospectionResponse, StandardRevocableToken, BasicRevocationErrorResponse>
 }
 
 impl PhabOAuthClient {
@@ -33,7 +34,7 @@ impl PhabOAuthClient {
             AuthUrl::new(format!("{}/oauthserver/auth/", phabricator_url))?,
             Some(TokenUrl::new(format!("{}/oauthserver/token/", phabricator_url))?)
         )
-        .set_redirect_url(RedirectUrl::new(redirect_url.clone())?);
+        .set_redirect_uri(RedirectUrl::new(redirect_url.clone())?);
 
         Ok(
             PhabOAuthClient {
@@ -49,23 +50,23 @@ impl PhabOAuthClient {
 
         let (auth_url, _csrf_token) = self.client
             .authorize_url(CsrfToken::new_random)
-            .set_redirect_url(Cow::Owned(url))
+            .set_redirect_uri(Cow::Owned(url))
             .url();
 
         Ok(auth_url)
     }
 
-    pub fn get_token(&self, code: String) -> Result<BasicTokenResponse> {
+    pub async fn get_token(&self, code: String) -> Result<BasicTokenResponse> {
         let token_response = self.client
             .exchange_code(AuthorizationCode::new(code))
-            .request(http_client)?;
+            .request_async(async_http_client).await?;
 
         Ok(token_response)
     }
 
-    pub fn get_user(&self, token: &AccessToken) -> Option<PhabricatorUser> {
+    pub async fn get_user(&self, token: &AccessToken) -> Option<PhabricatorUser> {
         let request_url = format!("{}/api/user.whoami?access_token={}", self.phabricator_url, token.secret());
-        let response = reqwest::blocking::get(request_url.as_str()).unwrap().text().unwrap();
+        let response = reqwest::get(request_url.as_str()).await.unwrap().text().await.unwrap();
         let user_result: OAuthResponse<PhabricatorUser>= serde_json::from_str(response.as_str()).unwrap();
         user_result.result
     }
--- src/user.rs
+++ src/user.rs
@@ -1,15 +1,13 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct PhabricatorUser {
     pub phid: String,
-    #[serde(rename(serialize = "userName", deserialize = "userName"))]
     pub user_name: String,
-    #[serde(rename(serialize = "realName", deserialize = "realName"))]
     pub real_name: String,
     pub image: String,
     pub uri: String,
     pub roles: Vec<String>,
-    #[serde(rename(serialize = "primaryEmail", deserialize = "primaryEmail"))]
     pub primary_email: String
 }
